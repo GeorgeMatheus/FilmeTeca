823457136a5ef54d8287e6391e63c257520cecb4
GeorgeMatheus <matheuss308@gmail.com>
2022-04-09 00:12:51 -0300

    Implementando axios no consumo da Api

--- FilmeTeca/src/components/Api.tsx
+++ FilmeTeca/src/components/Api.tsx
@@ -2,38 +2,37 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 
 
-// type Filme = {
-//   id: Number;
-//   title: String;
-//   overview: String;
-// } 
+type Filme = {
+  id: Number;
+  title: String;
+  overview: String;
+  poster_path: String;
+  vote_average: String;
+  release_date: String;
+
+} 
 
 export function Api() {
 
-  const [filmes, setFilmes] = useState([])
+  const [filmes, setFilmes] = useState<Filme[]>([])
   const image_path = 'https://image.tmdb.org/t/p/w500/'
 
-  // useEffect(() => {
-  //   axios.get('https://api-filmeteca.herokuapp.com/filmes/populares')
-  //     .then(response => {
-  //       setFilmes(response.data)
-  //     })
-  // }, [])
-
   useEffect(() => {
-
-    fetch('https://api-filmeteca.herokuapp.com/filmes/populares')
-    .then(response => response.json())
-    .then(data => setFilmes(data.results))
+    axios.get('https://api-filmeteca.herokuapp.com/filmes/populares')
+      .then(response => {
+        setFilmes(response.data.results)
+      })
   }, [])
 
   return (
     <ul>
       {filmes.map(filme => {
         return (
-          <li key={filme.id}>
+          <li key={filme.id.toString()}>
             <strong>{filme.title}</strong>
             <p>{filme.overview}</p>
+            <p>{filme.vote_average}</p>
+            <p>{filme.release_date}</p>
             <img src={`${image_path}${filme.poster_path}`}></img>
           </li>
         )
