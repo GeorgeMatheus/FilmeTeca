9b6a2e2cd8e41fffe0729385a9979cda0fec3a6f
Giuliano de Paulo Demoner <giuliano.demoner@gmail.com>
2022-06-16 14:30:10 -0300

    rodapé
    dividido em 3 partes, sendo
    1: à esquerda, mensagem de copyright
    2: ao centro, o nome do site
    3: à direita, links para redes sociais/contato

--- FilmeTeca/src/components/Rodape/index.tsx
+++ FilmeTeca/src/components/Rodape/index.tsx
@@ -1,12 +1,37 @@
-import React from "react"
+// import React from "react"
 import './style.scss'
+import { FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa" //importa os incones do instagram, youtube e twitter
+import { AiOutlineMail } from "react-icons/ai" //importa o incone de carta/email
 
 export function Rodape() {
   return(
 
     <footer>
-      <h4>Copyright Filmeteca</h4>
+      
+      <div className = "copyright">
+        Filmeteca © 2022
+      </div>
+
+      <div className = "logo">
+       Filmeteca
+      </div>
+
+      <div>
+        <a target="_blank" href="https://www.instagram.com/filmetecaoficial/">
+          <FaInstagram size = "40"/>
+        </a>
+        <a target="_blank" href="https://twitter.com/filmetecaoficial" >
+          <FaTwitter size = "40"/>
+        </a>
+        <a target="_blank" href="https://www.youtube.com/filmeteca">
+          <FaYoutube size = "40"/>
+        </a>
+        <a href="mailto:contato@filmeteca.com.br? Subject: Acessei seu Site&body=Muito Bom seu site.">
+          <AiOutlineMail size = "40"/>
+        </a>
+      </div>
+
     </footer>
-    
   )
 }
+
